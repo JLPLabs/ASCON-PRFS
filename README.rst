d8f1f976efef7f3875a9bf7832b35edd0c707d64
JLPLabs <4744024+JLPLabs@users.noreply.github.com>
2024-07-11 13:27:57 -0500

    layout adjustments

--- README.rst
+++ README.rst
@@ -4,3 +4,24 @@ ASCON-PRFS
 Demonstration of creating 8 byte tags using ASCON Psuedo-Random Function Short
 
 Note that the ASCON PRF "short" form means the input message to tag is <= 16 bytes and the tag is also <= 16 bytes.
+
+
+::
+
+  Demonstration of ASCON-PRFS
+
+  key        (hex) : 000102030405060708090a0b0c0d0e0f
+  base msg   (hex) : 4379636c6f6e65733230323421000000
+  ...........................................................
+  base msg (ascii) : [Cyclones2024!]
+
+  note: the last three bytes of the base message are updated with a nonce value
+  and then a tag generated.
+
+
+  nonce               msg (w/nonce)                     tag
+  ------------------  --------------------------------  -----------------
+    1000 (0x0003e8)  4379636c6f6e657332303234210003e8  51eef9b8733a73fb
+    20000 (0x004e20)  4379636c6f6e65733230323421004e20  ab4ad516d465047e
+  100000 (0x0186a0)  4379636c6f6e657332303234210186a0  6b96250a86382ac7
+  1000000 (0x0f4240)  4379636c6f6e657332303234210f4240  c11cdd72b7deead1
--- demo.c
+++ demo.c
@@ -14,35 +14,50 @@ int crypto_prf(unsigned char* out, unsigned long long outlen,
 
 void print_str(const char* label, const unsigned char* data, unsigned long long length);
 void print_bstr(const char* label, const unsigned char* data, unsigned long long length);
+void print_bstr_no(const unsigned char* data, unsigned long long length);
 
 
 int main(void) {
   unsigned char tag[8]  = {0, 0, 0, 0, 0, 0, 0, 0};
   unsigned char key[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-  unsigned char msg[16] = {'C', 'y', 'c', 'l', 'o', 'n', 'e', 's', '2', '0', '2', '4', ' ', 00, 00, 00};
+  unsigned char msg[16] = {'C', 'y', 'c', 'l', 'o', 'n', 'e', 's', '2', '0', '2', '4', '!', 00, 00, 00};
   int nonces[4] = {1000, 20000, 100000, 1000000};
 
-  printf("Demonstration of ASCON-PRFS\n");
+  printf("\n\nDemonstration of ASCON-PRFS\n\n");
+  print_bstr ("key        (hex) ", key, 16);
+  print_bstr("base msg   (hex) ", msg, 16);
+  printf("...........................................................\n");
   print_str ("base msg (ascii) ", msg, 16);
-  print_bstr("base msg (hex)   ", msg, 16);
   printf("\nnote: the last three bytes of the base message are updated with a nonce value\n");
-  printf("and then a tag generated.\n");
+  printf("and then a tag generated.\n\n\n");
 
+  printf("nonce               msg (w/nonce)                     tag\n");
+  printf("------------------  --------------------------------  -----------------\n");
   int i;
   for (i=0; i<4; i++) {
     msg[15] = nonces[i]         & 0xff;
     msg[14] = (nonces[i] >> 8)  & 0xff;
     msg[13] = (nonces[i] >> 16) & 0xff;
 
-    printf("nonces[%d] = %7d  ", i, nonces[i]); 
+
+    printf("%7d (%#08x)  ", nonces[i], nonces[i]); 
+    print_bstr_no(msg,16);
+    printf("  ");
     int ret = crypto_prf(tag, 8, msg, 16, key);
     if (ret == 0) {
-      print_bstr("tag = ", tag, 8);
+      print_bstr_no(tag, 8);
+      printf("\n");
     } else {
-      printf("error; ret = %d\n", ret);
+      printf("\nerror; ret = %d\n", ret);
     }
   }
   return 0;
+}
+
+// print as bytes; no label, newline, or colon
+void print_bstr_no(const unsigned char* data, unsigned long long length) {
+  unsigned long long i;
+  for (i = 0; i < length; i++) printf("%02x", data[i]);
 }
 
 // print as bytes
